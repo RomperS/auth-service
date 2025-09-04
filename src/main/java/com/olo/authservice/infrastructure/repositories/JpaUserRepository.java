a5b56a82bf7fccd1ac91442271056f3771a63af3
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-04 23:02:01 +0000

    feat(user): add UserMapper and UserRepositoryAdapter, include additional functions in JpaUserRepository for adapter

--- src/main/java/com/olo/authservice/infrastructure/repositories/JpaUserRepository.java
+++ src/main/java/com/olo/authservice/infrastructure/repositories/JpaUserRepository.java
@@ -1,7 +1,23 @@
 package com.olo.authservice.infrastructure.repositories;
 
 import com.olo.authservice.infrastructure.entities.UserEntity;
+import com.olo.permissions.Role;
+import com.olo.permissions.Title;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+
+    Optional<UserEntity> findByUsername(String username);
+
+    @Query("SELECT u FROM UserEntity u JOIN u.roles r WHERE r = :role")
+    List<UserEntity> findByRole(@Param("role") Role role);
+    @Query("SELECT u FROM UserEntity u JOIN u.titles t WHERE t = :title")
+    List<UserEntity> findByTitle(@Param("title") Title title);
 }
