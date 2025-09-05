3d900ea94455314489e4910a2f6c49f51345115e
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-05 06:18:34 +0000

    fix(user): correct use case implementation, was previously implementing the wrong one

--- src/main/java/com/olo/authservice/application/usecase/users/GetAllUsersImpl.java
+++ src/main/java/com/olo/authservice/application/usecase/users/GetAllUsersImpl.java
@@ -1,29 +1,28 @@
 package com.olo.authservice.application.usecase.users;
 
-import com.olo.authservice.domain.ports.inbound.users.GetAllUsersByTitlePort;
+import com.olo.authservice.domain.ports.inbound.users.GetAllUsersPort;
 import com.olo.authservice.domain.ports.outbound.UserRepositoryPort;
 import com.olo.authservice.domain.results.users.UserResult;
-import com.olo.permissions.Title;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-public class GetAllUsersImpl implements GetAllUsersByTitlePort {
+public class GetAllUsersImpl implements GetAllUsersPort {
 
     private final UserRepositoryPort userRepositoryPort;
 
     @Override
-    public List<UserResult> getAllUsersByTitle(Title title) {
+    public List<UserResult> getAllUsers() {
         return userRepositoryPort.findAll().stream()
                 .map(user ->
-                        new UserResult(
-                                user.id(),
-                                user.username(),
-                                user.email(),
-                                user.accountLocked(),
-                                new UserResult.PermissionsResult(user.titles(), user.roles())
-                        )
+                    new UserResult(
+                        user.id(),
+                        user.username(),
+                        user.email(),
+                        user.accountLocked(),
+                        new UserResult.PermissionsResult(user.titles(), user.roles())
+                    )
                 )
                 .toList();
     }
