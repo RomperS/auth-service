e2999d7ecab08f99f3063de2d49758bc1ead7447
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-05 07:35:27 +0000

    fix(security): replace addFilter with addFilterBefore in SecurityConfig

--- src/main/java/com/olo/authservice/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/olo/authservice/infrastructure/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -41,7 +42,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilter(jwtAuthFilter)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(customAuthenticationEntryPoint));
 
         return http.build();
