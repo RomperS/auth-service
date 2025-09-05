d130909730e74e35ae10cc6fb2bfdc9431e9ed17
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-05 06:24:42 +0000

    fix(user-service): inject implementations instead of ports to follow Clean Architecture

--- src/main/java/com/olo/authservice/application/service/UserService.java
+++ src/main/java/com/olo/authservice/application/service/UserService.java
@@ -3,6 +3,7 @@ package com.olo.authservice.application.service;
 import com.olo.authservice.domain.command.users.CreateUserCommand;
 import com.olo.authservice.domain.command.users.UpdateUserCommand;
 import com.olo.authservice.domain.ports.inbound.users.*;
+import com.olo.authservice.application.usecase.users.*;
 import com.olo.authservice.domain.results.users.UserResult;
 import com.olo.permissions.Role;
 import com.olo.permissions.Title;
@@ -23,64 +24,64 @@ public class UserService implements
         UnlockUserPort,
         UpdateUserPort {
 
-    private final CreateUserPort createUserPort;
-    private final DeleteUserPort deleteUserPort;
-    private final GetAllUsersByRolePort getAllUsersByRolePort;
-    private final GetAllUsersByTitlePort getAllUsersByTitlePort;
-    private final GetAllUsersPort getAllUsersPort;
-    private final GetUserByIdPort getUserByIdPort;
-    private final GetUserByUsernamePort getUserByUsernamePort;
-    private final LockUserPort lockUserPort;
-    private final UnlockUserPort unlockUserPort;
-    private final UpdateUserPort updateUserPort;
+    private final CreateUserImpl createUserImpl;
+    private final DeleteUserImpl deleteUserImpl;
+    private final GetAllUsersByRoleImpl getAllUsersByRoleImpl;
+    private final GetAllUsersByTitleImpl getAllUsersByTitleImpl;
+    private final GetAllUsersImpl getAllUsersImpl;
+    private final GetUserByIdImpl getUserByIdImpl;
+    private final GetUserByUsernameImpl getUserByUsernameImpl;
+    private final LockUserImpl lockUserImpl;
+    private final UnlockUserImpl unlockUserImpl;
+    private final UpdateUserImpl updateUserImpl;
 
     @Override
     public UserResult createUser(CreateUserCommand command) {
-        return createUserPort.createUser(command);
+        return createUserImpl.createUser(command);
     }
 
     @Override
     public void deleteUserById(Long id) {
-        deleteUserPort.deleteUserById(id);
+        deleteUserImpl.deleteUserById(id);
     }
 
     @Override
     public List<UserResult> getAllUsersByRole(Role role) {
-        return getAllUsersByRolePort.getAllUsersByRole(role);
+        return getAllUsersByRoleImpl.getAllUsersByRole(role);
     }
 
     @Override
     public List<UserResult> getAllUsersByTitle(Title title) {
-        return getAllUsersByTitlePort.getAllUsersByTitle(title);
+        return getAllUsersByTitleImpl.getAllUsersByTitle(title);
     }
 
     @Override
     public List<UserResult> getAllUsers() {
-        return getAllUsersPort.getAllUsers();
+        return getAllUsersImpl.getAllUsers();
     }
 
     @Override
     public UserResult getUserById(Long userId) {
-        return getUserByIdPort.getUserById(userId);
+        return getUserByIdImpl.getUserById(userId);
     }
 
     @Override
     public UserResult getUserByUsername(String username) {
-        return getUserByUsernamePort.getUserByUsername(username);
+        return getUserByUsernameImpl.getUserByUsername(username);
     }
 
     @Override
     public void lockUser(Long userId) {
-        lockUserPort.lockUser(userId);
+        lockUserImpl.lockUser(userId);
     }
 
     @Override
     public void unlockUser(Long userId) {
-        unlockUserPort.unlockUser(userId);
+        unlockUserImpl.unlockUser(userId);
     }
 
     @Override
     public UserResult updateUser(UpdateUserCommand command) {
-        return updateUserPort.updateUser(command);
+        return updateUserImpl.updateUser(command);
     }
 }
