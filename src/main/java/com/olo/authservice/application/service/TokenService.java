6570fd8dedf36552fdb5250cedeb98a31942e2cd
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-05 06:31:35 +0000

    fix(token-service): inject implementations instead of ports to follow Clean Architecture

--- src/main/java/com/olo/authservice/application/service/TokenService.java
+++ src/main/java/com/olo/authservice/application/service/TokenService.java
@@ -1,9 +1,7 @@
 package com.olo.authservice.application.service;
 
-import com.olo.authservice.domain.ports.inbound.tokens.CreateTokenPort;
-import com.olo.authservice.domain.ports.inbound.tokens.GenerateAccessTokenPort;
-import com.olo.authservice.domain.ports.inbound.tokens.GetActiveUserTokensPort;
-import com.olo.authservice.domain.ports.inbound.tokens.RevokeTokenPort;
+import com.olo.authservice.application.usecase.tokens.*;
+import com.olo.authservice.domain.ports.inbound.tokens.*;
 import com.olo.authservice.domain.results.tokens.AccessTokenResult;
 import com.olo.authservice.domain.results.tokens.TokenResult;
 import lombok.RequiredArgsConstructor;
@@ -13,28 +11,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TokenService implements CreateTokenPort, GenerateAccessTokenPort, GetActiveUserTokensPort, RevokeTokenPort {
 
-    private final CreateTokenPort createTokenPort;
-    private final GenerateAccessTokenPort generateAccessTokenPort;
-    private final GetActiveUserTokensPort getActiveUserTokensPort;
-    private final RevokeTokenPort revokeTokenPort;
+    private final CreateTokenImpl createTokenImpl;
+    private final GenerateAccessTokenImpl generateAccessTokenImpl;
+    private final GetActiveUserTokensImpl getActiveUserTokensImpl;
+    private final RevokeTokenImpl revokeTokenImpl;
 
     @Override
     public TokenResult createToken(String username) {
-        return createTokenPort.createToken(username);
+        return createTokenImpl.createToken(username);
     }
 
     @Override
     public AccessTokenResult generateAccessToken(String token) {
-        return generateAccessTokenPort.generateAccessToken(token);
+        return generateAccessTokenImpl.generateAccessToken(token);
     }
 
     @Override
     public List<TokenResult> getActiveUserToken(String username) {
-        return getActiveUserTokensPort.getActiveUserToken(username);
+        return getActiveUserTokensImpl.getActiveUserToken(username);
     }
 
     @Override
     public void revokeToken(String jti) {
-        revokeTokenPort.revokeToken(jti);
+        revokeTokenImpl.revokeToken(jti);
     }
 }
