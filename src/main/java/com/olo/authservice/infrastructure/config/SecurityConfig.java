abf6a025b54907e37d77cba2205216da90c8804b
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-05 05:44:03 +0000

    feat(security): add CustomAuthenticationEntryPoint and enhance SecurityConfig with JwtAuthFilter, SecurityFilterChain, and CustomUserDetailsService

--- src/main/java/com/olo/authservice/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/olo/authservice/infrastructure/config/SecurityConfig.java
@@ -1,15 +1,49 @@
 package com.olo.authservice.infrastructure.config;
 
+import com.olo.authservice.infrastructure.security.CustomAuthenticationEntryPoint;
+import com.olo.authservice.infrastructure.security.CustomUserDetailsService;
+import com.olo.authservice.infrastructure.security.JwtAuthFilter;
+import com.olo.authservice.infrastructure.security.JwtServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter(JwtServiceImpl jwtServiceImpl) {
+        return new JwtAuthFilter(jwtServiceImpl);
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+        http
+                .userDetailsService(userDetailsService)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/users/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilter(jwtAuthFilter)
+                .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(customAuthenticationEntryPoint));
+
+        return http.build();
     }
 }
